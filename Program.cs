ba7897907e70adf0ce731374eb361d5be04e2f88
Amit Patil <ampatilofficial49@gmail.com>
2024-10-24 09:48:10 +0530

    Changes : Practice some more LINQ query

--- Program.cs
+++ Program.cs
@@ -12,6 +12,28 @@ namespace LinqSheekenge
 
         public string Country { get; set; }
     }
+
+    public class Shipper
+    {
+        public int ShipperID { get; set; }
+
+        public string ShipperName { get; set;}
+    }
+
+    public class Order
+    {
+        //OrderID	CustomerID	EmployeeID	OrderDate	ShipperID
+        public int OrderID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        public string OrderDate { get;set; }
+
+        public int ShipperID { get; set; }
+    }
+
     public class Program
     {
         public static IEnumerable<Customer> GetCustomers()
@@ -30,9 +52,33 @@ namespace LinqSheekenge
             };
         }
 
+        public static IEnumerable<Shipper> GetShippers() 
+        {
+            return new List<Shipper>()
+            {
+                new Shipper(){ ShipperID = 1, ShipperName = "Speedy Express"},
+                new Shipper(){ ShipperID = 2, ShipperName = "United Package"},
+                new Shipper(){ ShipperID = 3, ShipperName = "Federal Shipping"},
+            };
+        }
+
+        public static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() { OrderID = 10248, CustomerID = 90, EmployeeID = 5, OrderDate = "1996-07-04", ShipperID = 3 },
+                new Order() { OrderID = 10249, CustomerID = 81, EmployeeID = 6, OrderDate = "1996-07-05", ShipperID = 1 },
+                new Order() { OrderID = 10250, CustomerID = 34, EmployeeID = 4, OrderDate = "1996-07-08", ShipperID = 2 },
+            };
+        }
+
         static void Main(string[] args)
         {
             var LstofCustomer = GetCustomers();
+
+            var LstOfShipper = GetShippers();
+
+            var LstOfOrders = GetOrders();
 
             /*
              * The following SQL statement lists the number of customers in each country:
@@ -79,6 +125,24 @@ namespace LinqSheekenge
                 Console.WriteLine(result.id + " " + result.rowinCustomers);
             }
 
+            /*
+             * SELECT Shippers.ShipperName, COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
+                LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+                GROUP BY ShipperName;
+             */
+
+            /*
+            var newResult = from Order in LstOfOrders//shipperId in LstOfShipper
+                            join
+                            shipper in LstOfShipper
+                            on
+                            Order.ShipperID equals shipper.ShipperID
+                            group shipper.ShipperName into g
+                            orderby g.Select(x => x.sh)
+                            select new { 
+
+                            }
+            */
         }
     }
 }
